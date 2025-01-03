9e6a56b28d4f9a2f662b0f121f3ebe8b3bf9c11c
Ryan Conneely <ryc_127@proton.me>
2025-01-03 22:41:05 +0000

    added dynamic initialisation of chessboard and changed the mainpage along with its colours

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,9 +2,56 @@
 {
     public partial class MainPage : ContentPage
     {
+        private int boardHeight = 8;
+        private int boardLength = 8;
+        private int[,] chessBoard;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void InitializeTheGame_Clicked(object sender, EventArgs e)
+        {
+            chessBoard = new int[boardHeight, boardLength];
+
+            //initialises amount of rows and cols
+            for (int i = 0; i < boardHeight; i++)
+            {
+                visibleChessBoard.AddRowDefinition(new RowDefinition());
+            }
+            for (int i = 0; i < boardLength; i++)
+            {
+                visibleChessBoard.AddColumnDefinition(new ColumnDefinition());
+            }
+
+            //makes the board visible
+            for (int i = 0; i < boardHeight; i++)
+            {
+                for (int j = 0; j < boardLength; j++)
+                {
+                    Border squareStyle = new Border()
+                    {
+                        BackgroundColor = Colors.Orange,
+
+                    };
+                    visibleChessBoard.Add(squareStyle, i, j);
+
+                    Label label = new Label()
+                    {
+                        //Text = chessBoard[i, j].ToString(),
+                    };
+                    visibleChessBoard.Add(label, i, j);
+
+                    Button square = new Button()
+                    {
+                        BackgroundColor = Colors.Orange,
+                        //Text = chessBoard[i, j].ToString(),
+                    };
+
+                    visibleChessBoard.Add(square, i, j);
+                }
+            }
         }
 
     }
