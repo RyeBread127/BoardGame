fa766a5537cbf36983fabb53165cd1c7c041f7ac
Ryan Conneely <ryc_127@proton.me>
2025-01-07 22:33:22 +0000

    changed UI for options and settings

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,10 +2,6 @@
 {
     public partial class MainPage : ContentPage
     {
-        private int boardHeight = 11;
-        private int boardLength = 11;
-        private int[,] board;
-
         public MainPage()
         {
             InitializeComponent();
